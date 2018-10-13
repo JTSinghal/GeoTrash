c135ceaab5dc61cae794ed90ac1600e41be9ca98
JTSinghal <jatinsinghal19@gmail.com>
2018-10-13 19:10:25 -0400

    Routing system actually complete this time :)

--- public/script.js
+++ public/script.js
@@ -1,6 +1,13 @@
 var materials = ["Paper/Cardboard", "Plastic", "Metals", "Glass", "Batteries/Bulbs", "Miscellaneous Electronics"]
+var activeMap = null;
 
 function route(){
+	//var routing = null;
+	if (activeMap){
+		routing.spliceWaypoints(0, 2);
+	}
+	//routing.spliceWaypoints(0, 2);
+
 	var recycleType = null;
 	if ($('#findDropdown').val() === "paper"){
 		recycleType = overlays[materials[0]];
@@ -20,31 +27,23 @@ function route(){
 	recycleType.eachLayer(function(l){			//debug by c
 		//var dist = L.latLng(curr.lat, curr.lng).distanceTo(l.getLatLng())/1000;
 		var dist = L.latLng([curr.lat, curr.lng]).distanceTo(l.getLatLng())/1000;
-		console.log("Dist pushed: " + dist);
 		distances.push(dist);
 	});
 	var indexOfShortest = 0		//debug: create dummy pins around campus that accept a certain recycling type. Change dropdown to that recycling type and click route and console log the different distance and the shortest distance and the pin coordinates of that pin.
 	for (var i = 0; i < distances.length; i++){
-		console.log("Shortest: " + distances[indexOfShortest]);
-		console.log("Current : " + distances[i]);
-		console.log("Index: " + indexOfShortest);
 		if (distances[i] < distances[indexOfShortest]){
 			indexOfShortest = i;	//access using getLayer(id)
-			console.log("Switc: " + indexOfShortest);
 		}
 	}
 
-	//console.log(L.latlng(Object.keys(recycleType._layers)[indexOfShortest]._latlng[0], Object.keys(recycleType._layers)[indexOfShortest]._latlng[1]))
-	console.log(Object.keys(recycleType._layers)[indexOfShortest]);
-	L.Routing.control({
+	routing = L.Routing.control({
 		waypoints: [
 			L.latLng([curr.lat, curr.lng]),
-			//recycleType.getLayer(indexOfShortest + 26).getLatLng()	//getLayer(i) is returning something undefined, should return a layer. Adding 25 made it grab 29
-			//L.latlng(Object.keys(recycleType._layers)[indexOfShortest]._latlng[0], Object.keys(recycleType._layers)[indexOfShortest]._latlng[1])
 			recycleType.getLayer(Object.keys(recycleType._layers)[indexOfShortest]).getLatLng()
 		],
 		routeWhileDragging: true
 	}).addTo(map)
+	activeMap = true;
 }
 
 function addBin(){
