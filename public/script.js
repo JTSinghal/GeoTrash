9f2356cf8d17661bf288563cbbead40110a4666f
JosephMBarr <barrjoeym@mgail.com>
2018-10-12 21:19:07 -0400

    PUSH works

---
+++ public/script.js
@@ -0,0 +1,7 @@
+//Test post request
+$.post("/add",  {
+    lat: 12.3456,
+    lon: 123.4567,
+    code: 4,
+    floor: -1
+});
--- server.py
+++ server.py
@@ -1,33 +1,39 @@
+#!/usr/bin/env python3
 import tornado.ioloop
-import tornado.web
+import tornado.web as web
 import mysql.connector
+import os
 
 db = mysql.connector.connect(
     host="localhost",
     user="geotrash",
-    passwd="ifeelfree"
+    passwd="ifeelfree",
+    database="geotrash"
 )
 c = db.cursor()
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render("index.html", title="GeoTrash")
+public_root = os.path.join(os.path.dirname(__file__), 'public')
 
-class PostHandler(tornado.web.RequestHandler):
+class MainHandler(web.RequestHandler):
+    def get(self):
+        self.render("geotrash.html", title="GeoTrash")
+
+class PostHandler(web.RequestHandler):
     def post(self):
         lat = self.get_argument('lat')
         lon = self.get_argument('lon')
         code = self.get_argument('code')
         floor = self.get_argument('floor')
-        sql = "INSERT INTO bins (lat, lon, code, floor) values (%f, %f, %d, %d)"
+        sql = "INSERT INTO bins (lat, lon, code, floor) values (%s, %s, %s, %s)"
         val = (lat, lon, code, floor)
         c.execute(sql, val)
         db.commit()
 
 def make_app():
 
-    return tornado.web.Application([
+    return web.Application([
         (r"/add", PostHandler),
         (r"/", MainHandler),
+        (r'/(.*)', web.StaticFileHandler, {'path': public_root})
     ])
 def main():
     app = make_app()
