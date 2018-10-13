fbfdf11d6e8a0854567f7c6ebb6550a980e2f82d
JeroSik <yoon161@purdue.edu>
2018-10-13 09:17:22 -0400

    Fixed current location

--- js/map.js
+++ js/map.js
@@ -1,4 +1,8 @@
 var map = L.map('map').setView([40.4268, -86.9195], 18);
+var curr = {
+  lat: '',
+  lng: ''
+};
 
 // Run on startup
 initMap();
@@ -8,19 +12,30 @@ findLocation();
 function initMap() {
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 22,
+    maxZoom: 25,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiamVyb3NpayIsImEiOiJjam42cjAzcnQwMjFkM3Jtc2N2M2V3NDZwIn0.pL85YX_yr3huGKt-Nz4jPg'
   }).addTo(map);
 }
 
-// Find current location
+// Finds current location
 function findLocation() {
-  var lat, lng;
+  // var lat, lng;
   var getlatlng = function(position) {
-    lat = position.coords.latitude;
-    lng = position.coords.longitude;
-    	L.marker([lat, lng]).addTo(map).bindPopup("You are here!").openPopup();
+    curr.lat = position.coords.latitude;
+    curr.lng = position.coords.longitude;
+    L.marker([curr.lat, curr.lng]).addTo(map).bindPopup("You are here!").openPopup();
   };
   navigator.geolocation.getCurrentPosition(getlatlng);
 }
+
+// Recycle icon
+var recycleIcon = L.Icon.extend({
+    options: {
+        iconSize:     [38, 95],
+        shadowSize:   [50, 64],
+        iconAnchor:   [22, 94],
+        shadowAnchor: [4, 62],
+        popupAnchor:  [-3, -76]
+    }
+});
