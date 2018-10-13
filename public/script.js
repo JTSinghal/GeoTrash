5b00508b6c602cb9ed05c9c02994613f2fb875ca
JosephMBarr <barrjoeym@mgail.com>
2018-10-12 22:04:42 -0400

    Code calculation

--- public/script.js
+++ public/script.js
@@ -1,7 +1,20 @@
-//Test post request
-/*$.post("/add",  {
-    lat: 12.3456,
-    lon: 123.4567,
-    code: 4,
-    floor: -1
-});*/
+
+
+
+function addBin(){
+    var code = 0;
+    
+    $('.whatCanBox').each(function(){
+        if($(this).prop('checked') == true){
+            code += parseInt($(this).val());
+        }
+    });
+    console.log(code);
+    //Test post request
+    /*$.post("/add",  {
+        lat: 12.3456,
+        lon: 123.4567,
+        code: 4,
+        floor: -1
+    });*/
+}
